3f90c753c82a23eff592c72b31a02fa7485cbaf3
Iris <147082102+ii004@users.noreply.github.com>
2024-04-28 23:41:36 +0100

    Implemented the pull string function
    - imported the flight chair to make it more immersive
    - implemented the pull string function to pull the trigger

---
+++ Assets/Script/String.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class String : MonoBehaviour
+{
+    [SerializeField] private Transform endpoint_1, endpoint_2;
+
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    public void CreateString(Vector3? midPosition)
+    {
+        Vector3[] linePoints = new Vector3[midPosition == null ? 2 : 3];
+        linePoints[0] = endpoint_1.localPosition;
+        if (midPosition != null)
+        {
+            linePoints[1] = transform.InverseTransformPoint(midPosition.Value);
+        }
+        linePoints[^1] = endpoint_2.localPosition;
+
+        lineRenderer.positionCount = linePoints.Length;
+        lineRenderer.SetPositions(linePoints);
+    }
+
+    void Update()
+    {
+        CreateString(null);
+    }
+}
